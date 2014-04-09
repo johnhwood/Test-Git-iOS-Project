795d9e9d66fb77edba6bfc6176d0ba1aab31da70
John H Wood <johnhwood@192-168-1-4.tpgi.com.au>
2014-04-09 14:02:59 +1000

    add test class

---
+++ Git Test/JHWTest.h
@@ -0,0 +1,13 @@
+//
+//  JHWTest.h
+//  Git Test
+//
+//  Created by John H Wood on 9/04/2014.
+//  Copyright (c) 2014 John H Wood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JHWTest : NSObject
+
+@end
---
+++ Git Test/JHWTest.m
@@ -0,0 +1,13 @@
+//
+//  JHWTest.m
+//  Git Test
+//
+//  Created by John H Wood on 9/04/2014.
+//  Copyright (c) 2014 John H Wood. All rights reserved.
+//
+
+#import "JHWTest.h"
+
+@implementation JHWTest
+
+@end
